affaf77e1412f93d69a1dc9cd1cd19bd7a922df8
AchrafAtt <G134219290@taalim.ma>
2024-12-14 08:47:30 +0100

    try dockerized this

--- streamlit-service/intrface.py
+++ streamlit-service/intrface.py
@@ -9,7 +9,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
     files = {"file": uploaded_file.getvalue()}
-    response = requests.post("http://localhost:8000/ocr/", files=files)
+    response = requests.post("http://fastapi:8000/ocr/", files=files)
 
     if response.status_code == 200:
         st.subheader("Extracted Text:")
